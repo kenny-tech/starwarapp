2b006fbb5365e43dee02daa1fbd9e2def09aad8f
kennytech <kennyemma2008@gmail.com>
2022-09-14 13:53:47 +0100

    Updated homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,10 @@ import { Navbar, Nav, Container, Form } from 'react-bootstrap';
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
-            <Container>
             <Navbar.Brand href="#home">Starwar App</Navbar.Brand>
             <Nav className="me-auto">
                 <Form.Control type="text" placeholder="Search" style={{width: "400px"}} />
             </Nav>
-            </Container>
       </Navbar>
     )
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,19 +5,21 @@ import Sidebar from './Sidebar';
 
 const Homepage = () => {
     return (
-        <Row>
+        <React.Fragment>
             <Header />
-            <Col md={3}>
-                <div className="mt-2 ml-2">
-                    <Sidebar/>
-                </div>
-            </Col>
-            <Col md={9}>
-                <div style={{margin: '20px'}}>
-                    <h1>This is the homepage</h1>
-                </div>
-            </Col>
-        </Row>
+            <Row>
+                <Col md={3}>
+                    <div className="mt-2 ml-2">
+                        <Sidebar/>
+                    </div>
+                </Col>
+                <Col md={9}>
+                    <div style={{margin: '20px'}}>
+                        <h1>This is the homepage</h1>
+                    </div>
+                </Col>
+            </Row>
+        </React.Fragment>
     )
 }
 
