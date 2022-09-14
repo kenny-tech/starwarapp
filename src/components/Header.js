90d28cbc6367ff4c0f6b6bcc6ff469f88aecd707
kennytech <kennyemma2008@gmail.com>
2022-09-14 13:15:05 +0100

    Created Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, Nav, Container, Form } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Starwar App</Navbar.Brand>
+            <Nav className="me-auto">
+                <Form.Control type="text" placeholder="Search" style={{width: "500px"}} />
+            </Nav>
+            </Container>
+      </Navbar>
+    )
+}
+
+export default Header;
