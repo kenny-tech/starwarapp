5369336d9c000965250317def978cacc4d9010e5
kennytech <kennyemma2008@gmail.com>
2022-09-15 12:01:38 +0100

    Implementing add to favourite

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, Table } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom'
+import { Star, StarFill } from 'react-bootstrap-icons';
 
 import Header from './Header';
 import Sidebar from './Sidebar';
@@ -10,6 +11,8 @@ const Details = () => {
     const location = useLocation();
     const { name } = location.state;
     const [person, setPerson] = useState([]);
+    const [isFavourite, setIsFavourite] = useState(false);
+    const [favourite, setFavourite] = useState([]);
 
     useEffect(() => {
         getPerson();
@@ -19,6 +22,21 @@ const Details = () => {
         let data = JSON.parse(localStorage.getItem("people")).find(person => person.name === name);
         console.log(data);
         setPerson(data);
+    }
+
+    const handleFavouriteClick = (name) => {
+        try {
+            setIsFavourite(!isFavourite);
+            
+            if(!favourite.includes(name)){          // check whether name has been added to favourite
+                favourite.push(name);               // add to favourite because name doesn't exists
+            }else{
+                favourite.splice(favourite.indexOf(name), 1);  // remove from favourite
+            }
+            localStorage.setItem("favourites", JSON.stringify(favourite));
+        } catch (error) {
+            console.log(error);
+        } 
     }
 
     return (
@@ -32,7 +50,7 @@ const Details = () => {
                 </Col>
                 <Col md={9}>
                     <div style={{margin: '20px'}}>
-                        <h2>{name}</h2>
+                        <h2>{name} <span onClick={() => handleFavouriteClick(person.name)}>{isFavourite? <StarFill color="royalblue" size={30} />: <Star color="royalblue" size={30} />}</span></h2>
                         <Table striped>
                             <tbody>
                                 <tr>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Star, StarFill } from 'react-bootstrap-icons';
 
 const Sidebar = () => {
+
+    const [isFavourite, setIsFavourite] = useState(false);
+    const [favourite, setFavourite] = useState([]);
+    let favourites = JSON.parse(localStorage.getItem("favourites"));
+
+    const handleFavouriteClick = (name) => {
+        try {
+            setIsFavourite(!isFavourite);
+            
+            if(!favourite.includes(name)){          // check whether name has been added to favourite
+                favourite.push(name);               // add to favourite because name doesn't exists
+            }else{
+                favourite.splice(favourite.indexOf(name), 1);  // remove from favourite
+            }
+            localStorage.setItem("favourites", JSON.stringify(favourite));
+        } catch (error) {
+            console.log(error);
+        } 
+    }
+
+    
     return (
         <div style={{margin: '20px'}}>
             <h3>Favourites</h3>
+            {
+                favourites!==null ? null : <p>No Favourite found</p>
+            }
+            {
+                favourites !== null? favourites.map(favourite => {
+                    return (
+                        <p>{favourite} <span onClick={() => handleFavouriteClick(favourite)}>{favourite? <StarFill color="royalblue" size={15} />: <Star color="royalblue" size={15} />}</span></p>
+                    )
+                }) : null
+            }
         </div>
     )
 }
